c06417f83ca11166fdab388d7e10d8e69d291e0c
Chinedu Ejibendu <chinextworld@yahoo.com>
2021-05-10 06:20:56 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'webdoctor' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'ejibendu' );
+define( 'DB_PASSWORD', '*********************' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
